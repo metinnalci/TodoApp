e64df9c56f3369ae8be5560a352ada6494dac26d
Metin NALCI <metinnalci@outlook.com>
2021-10-10 03:34:42 +0300

    Add project files.

---
+++ Controllers/TodoController.cs
@@ -0,0 +1,63 @@
+﻿using CSD.TodoApplicationRestApp;
+using CSD.TodoApplicationRestApp.Factory;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _001_TodoApplicationRestApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TodoController : ControllerBase
+    { 
+        [HttpGet("all")]
+        public IEnumerable<TodoInfo> FindAll()
+        {
+            var todoRandomFactory = new TodoRandomFactory();
+
+            return todoRandomFactory.All;
+        }
+
+        [HttpGet("random")]
+        public TodoInfo GetRandom()
+        {
+            var todoRF = new TodoRandomFactory();
+
+            return todoRF.RandomTodo;
+        }
+
+        [HttpGet("deneme")]
+        public IEnumerable<TodoInfo> GetRandoms(string count)
+        {
+
+
+            return int.TryParse(count, out int result) ? GetRandoms(result) : new List<TodoInfo>();
+        }
+
+        [HttpGet("randoms")]
+        public IEnumerable<TodoInfo> GetRandoms(int count)
+        {
+            var todoRF = new TodoRandomFactory();
+            var todos = new List<TodoInfo>();
+
+            for (int i = 0; i < count; i++)
+            {
+                todos.Add(todoRF.RandomTodo);
+            }
+
+            return todos;
+        }
+
+        [HttpGet("find")]
+        public IEnumerable<TodoInfo> FindTodoByTitle(string title)
+        {
+            var t = new TodoRandomFactory();
+
+            return t.FindTodosByTitleContains(title);
+
+        }
+    }
+}
---
+++ Factory/TodoRandomFactory.cs
@@ -0,0 +1,54 @@
+﻿using CSD.TodoApplicationRestApp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CSD.TodoApplicationRestApp.Factory
+{
+    public class TodoRandomFactory
+    {
+        private List<TodoInfo> m_todos;
+        private Random m_random = new();
+
+        private void loadTodos()
+        {
+            m_todos = new List<TodoInfo>
+            {
+                new TodoInfo{Title = "Eczane", Text = "Vitamin, Aspirin"},
+                new TodoInfo{Title = "Market", Text = "Gofret, Biberon"},
+                new TodoInfo{Title = "Okul", Text = "Intro ro prog, Software Eng."},
+                new TodoInfo{Title = "SüperMarket", Text = "Çay"}
+
+            };
+        }
+
+        public TodoRandomFactory()
+        {
+            loadTodos();
+        }
+
+        public IEnumerable<TodoInfo> All => m_todos;
+
+        public TodoInfo RandomTodo => m_todos[m_random.Next(m_todos.Count)];
+
+
+        public IEnumerable<TodoInfo> FindTodosByTitleContains(string title)
+        {
+            var list = new List<TodoInfo>();
+
+            foreach (var item in m_todos)
+            {
+                if (item.Title.Contains(title))
+                {
+                    list.Add(item);
+                }
+            }
+
+            return list;
+
+        }
+
+
+    }
+}
